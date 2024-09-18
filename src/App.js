56672f30ed1b1a973b52365df8305dd9c232ef77
jorge <jcanomelero@gmail.es>
2024-09-18 16:43:39 +0200

    primer commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+
+
+
+
+
+  return (
+    <div className="App">
+      <h1>Catálogo de películas</h1>
+    </div>
+  );
+}
+
+export default App;
