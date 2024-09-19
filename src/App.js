900338aecda117de8bad2580a7e6b113cd4dce71
jorge <jcanomelero@gmail.es>
2024-09-19 12:34:37 +0200

    haciendo inicio

--- src/App.js
+++ src/App.js
@@ -11,7 +11,36 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Catálogo de películas</h1>
+      <div id="header" className="w-100 h-auto d-flex justify-content-center align-items-center">
+        <div className="w-75 d-flex justify-content-between align-items-center">
+          <div className="w-auto d-flex justify-content-between align-items-center">
+            <img src="img/paramount-plus-logo-0.png" style={{width: '100px'}}></img>
+            <h1 className="fs-2 text-wrap ms-5">Catálogo Películas</h1>
+          </div>
+          
+          <div className="w-25 d-flex justify-content-star align-items-center">
+            <nav className="w-100 d-flex justify-content-between align-items-center flex-wrap">
+              <div>
+                <a className="nav-link fs-5" aria-current="page" href="../index.html">Inicio</a>
+              </div>
+              <div>
+                <a className="nav-link fs-5" href="#">Películas</a>
+              </div>
+              <div>
+                <a className="nav-link fs-5" href="#">Series</a>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+      <div className="w-100 border border-black" style={{height: '500px'}}>
+
+      </div>
+
+      <div id="footer" className="w-100 border border-black" style={{height: '30px'}}>
+
+      </div>
     </div>
   );
 }
