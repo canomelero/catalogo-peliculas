2241a16c70c5caa2648b00190fe7e44661e11d12
jorge <jcanomelero@gmail.es>
2025-03-21 16:44:45 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -2,8 +2,37 @@
 const btnComentarios = document.getElementById("btnComents");
 const comentsSection = document.getElementById("coments");
 
-// Por cada vez que se pulsa el botón, se va a ir añadiendo o quitando la clase hidden que permite
-// que se oculte o se visualice la sección de comentarios
-btnComentarios.addEventListener('click', function() {
+// Se obtiene el botón de enviar del formulario y el bloque donde se añaden
+const btnEnviar = document.getElementById("btnEnviar");
+const comentSection = document.getElementById("listComents");
+
+// Por cada vez que se pulsa el botón, se va a ir añadiendo o quitando la clase (toggle) "hidden",
+// permitiendo que se oculte o se visualice la sección de comentarios
+btnComentarios.addEventListener('click', function () {
     comentsSection.classList.toggle('hidden');
-})
+});
+
+
+btnEnviar.addEventListener('click', function (e) {
+    e.preventDefault();
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let coment = document.getElementById("txtComent").value;
+
+    comentSection.insertAdjacentHTML("beforeend", `
+        <div class="coment">
+            <div>
+                <img src="../img/usuario.webp" alt="usuario">
+            </div>
+    
+            <p>Autor: ${name}</p>
+            <p>Fecha: 21/03/2025</p>
+            <p>Hora: 19:30 </p>
+            <p>${coment}</p>
+        </div >
+    `);
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("txtComent").value = "";
+});
