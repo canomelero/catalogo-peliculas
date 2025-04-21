6d93ca157bceb45eb06ac28571c3bbef2a3728ad
Jorge <canomelero1@gmail.com>
2025-04-21 13:38:42 +0200

    acabada

--- practica3/imprimir.php
+++ practica3/imprimir.php
@@ -1,8 +1,24 @@
 <?php
     require_once __DIR__ . '/vendor/autoload.php';
+    require_once __DIR__ . '/modelos/peliculaMod.php';
 
     $loader = new \Twig\Loader\FilesystemLoader('templates');
     $twig = new \Twig\Environment($loader);
 
-    echo $twig->render('pelicula_imprimir.html', []);
+    // Se obtiene la información de la película y sus imagenes
+    $idPelicula = (int) $_GET["id"];
+    $pelicula = PeliculaModelo::getPelicula($idPelicula);
+    $imgs = PeliculaModelo::getImagenes($idPelicula);
+
+    // Si hay algún dato de la película que es null, se redirige a una página con error 400
+    if($pelicula == [] || $imgs == []) {
+        http_response_code(404);
+        echo "Error 404: falta información de la película en la Base de Datos";
+        exit();
+    }
+
+    echo $twig->render('pelicula_imprimir.html', [
+        'pelicula' => $pelicula,
+        'imgs' => $imgs
+    ]);
 ?>
