cf766af487f5cd9b00ddf9de58b62099c6d953de
Jorge <canomelero1@gmail.com>
2025-05-21 15:22:53 +0200

    corriengo las imgs

--- practica4/agregar_peli.php
+++ practica4/agregar_peli.php
@@ -9,6 +9,7 @@
     $usuarioLog = isset($_SESSION['usuarioAct']) ? $_SESSION['usuarioAct'] : '';
     $rol = isset($_SESSION['rol']) ? $_SESSION['rol'] : '';
     $mensaje = "";
+    $img = "";
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
         $titulo = isset($_POST["titulo"]) ? $_POST["titulo"] : '';
@@ -20,35 +21,35 @@
         $dt = DateTime::createFromFormat('Y-m-d', $fecha);   // Para validar que la fecha es correcta
         $hashtag = isset($_POST["hashtag"]) ? $_POST["hashtag"] : '';
 
-        if(isset($_FILES['imagen'])){
-            $errors= array();
-            $file_name = $_FILES['imagen']['name'];
-            $file_size = $_FILES['imagen']['size'];
+        if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] === UPLOAD_ERR_OK) {
             $file_tmp = $_FILES['imagen']['tmp_name'];
-            $file_type = $_FILES['imagen']['type'];
-            $file_ext = strtolower(end(explode('.',$_FILES['imagen']['name'])));
-            
-            $extensions= array("jpeg","jpg","png");
-            
-            if (in_array($file_ext,$extensions) === false){
-              $errors[] = "Extensión no permitida, elige una imagen JPEG o PNG.";
-            }
-            
-            if ($file_size > 2097152){
-              $errors[] = 'Tamaño del fichero demasiado grande';
-            }
-            
-            if(empty($errors)==true) {
-              move_uploaded_file($file_tmp, "img/" . $file_name);
-              
-              $varsParaTwig['imagen'] = "img/" . $file_name;
+            $file_name = basename($_FILES['imagen']['name']);
+            $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 
-              $img = "./img/" . $file_name;
+            $allowed_exts = ["jpg", "jpeg", "png"];
+            $max_size = 2 * 1024 * 1024; // 2 MB
+
+            if (!in_array($file_ext, $allowed_exts)) {
+                $mensaje = "Extensión no permitida. Usa jpg, jpeg o png.";
+            } elseif ($_FILES['imagen']['size'] > $max_size) {
+                $mensaje = "Tamaño de archivo demasiado grande (máx 2MB).";
+            } else {
+                $upload_dir = __DIR__ . "/img/";
+                $new_name = uniqid("peli_") . "." . $file_ext;
+                $destination = $upload_dir . $new_name;
+
+                if (move_uploaded_file($file_tmp, $destination)) {
+                    $img = "img/" . $new_name;
+                } else {
+                    $mensaje = "Error al guardar la imagen.";
+                    $mensaje .= "<br>Temp file: $file_tmp";
+                    $mensaje .= "<br>Destination: $destination";
+                    $mensaje .= "<br>¿Existe carpeta?: " . (is_dir($upload_dir) ? 'Sí' : 'No');
+                    $mensaje .= "<br>¿Es escribible?: " . (is_writable($upload_dir) ? 'Sí' : 'No');
+                }
             }
-            
-            if (sizeof($errors) > 0) {
-              $varsParaTwig['errores'] = $errors;
-            }
+        } else if (isset($_FILES['imagen']) && $_FILES['imagen']['error'] != UPLOAD_ERR_NO_FILE) {
+            $mensaje = "Error en la subida: código " . $_FILES['imagen']['error'];
         }
 
         // Si no se ha podido crear el objeto porque la fecha dada y el formato puesto no coinciden, devuelve false
@@ -62,6 +63,11 @@
                                         $descripcion, $fecha, $img, $hashtag);
             header("Location: portada.php");
             exit();    
+        }
+        else {
+            if (empty($mensaje)) {
+                $mensaje = "Todos los campos son obligatorios.";
+            }
         }
     }
 
